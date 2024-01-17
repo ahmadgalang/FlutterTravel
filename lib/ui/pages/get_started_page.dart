e4b8901a47559c8f1f8f3510e0c831998ff69146
Ahmad Galang <258galang@gmail.com>
2024-01-17 21:55:58 +0700

    Create new file get started

---
+++ lib/ui/pages/get_started_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import '../../shared/theme.dart';
+
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/background.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
