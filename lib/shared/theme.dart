08422c0b1e291fef350716e83d8ed20e8c3753f9
Ahmad Galang <258galang@gmail.com>
2024-01-24 21:24:45 +0700

    Change routes

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -10,6 +10,7 @@ Color kRedColor = const Color(0xffEB70A5);
 Color kBackgroundColor = const Color(0xffFAFAFA);
 Color kInactiveColor = const Color(0xffDBD7EC);
 Color kActiveColor = const Color(0xff5C40CC);
+Color kSecondaryColor = const Color(0xff1F1449);
 
 TextStyle blackTextStyle = GoogleFonts.poppins(color: kBlackColor);
 TextStyle whiteTextStyle = GoogleFonts.poppins(color: kwhiteColor);
@@ -17,6 +18,7 @@ TextStyle greyTextStyle = GoogleFonts.poppins(color: kGreyColor);
 TextStyle greenTextStyle = GoogleFonts.poppins(color: kGreenColor);
 TextStyle redTextStyle = GoogleFonts.poppins(color: kRedColor);
 TextStyle primaryTextStyle = GoogleFonts.poppins(color: kPrimaryColor);
+TextStyle secondaryTextStyle = GoogleFonts.poppins(color: kSecondaryColor);
 
 FontWeight light = FontWeight.w300;
 FontWeight regular = FontWeight.w400;
@@ -25,3 +27,6 @@ FontWeight semiBold = FontWeight.w600;
 FontWeight bold = FontWeight.w700;
 FontWeight extraBold = FontWeight.w800;
 FontWeight black = FontWeight.w900;
+
+double defaultMargin = 24.0;
+double defaultRadius = 17.0;
